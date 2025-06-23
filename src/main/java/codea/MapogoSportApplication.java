0f0758f9021c6d264871d0d91f673cd6c0a6945d
Phi Hung <phihung.devka@gmail.com>
2025-06-23 11:07:05 +0700

    fix enviroment variables

--- src/main/java/codea/MapogoSportApplication.java
+++ src/main/java/codea/MapogoSportApplication.java
@@ -3,10 +3,13 @@ package codea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MapogoSportApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./").ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {System.setProperty(entry.getKey(), entry.getValue());});
 		SpringApplication.run(MapogoSportApplication.class, args);
 	}
 
